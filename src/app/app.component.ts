6d015324cf5556b7fa6389bdb9c7f8fedff119d9
Enrique Fernández Pérez <enrique.fernandez@bosonit.com>
2024-04-25 14:14:44 +0200

    subir

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = '11-directiveSignals';
+  
 }
