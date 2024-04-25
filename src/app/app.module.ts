065b73e4c915e8899c9e2662ab18661bc146a40d
Enrique Fernández Pérez <enrique.fernandez@bosonit.com>
2024-04-25 14:11:38 +0200

    subir

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CountriesModule } from './countries/countries.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+
+    CountriesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
